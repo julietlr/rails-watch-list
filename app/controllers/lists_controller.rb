e99ca12f40263cd277ac5b3bbd65d37dc2479a2c
Julie Tailleur <julie.tailleur@gmail.com>
2022-11-18 11:58:42 +0100

    linux

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,22 @@
+class ListsController < ApplicationController
+  def index
+    @lists = List.all
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    @list.movie = @movie
+    @movies
+  end
+
+  private
+  def list_params
+    params.require(:list).permit(:name)
+  end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  get 'movies/index'
+  get 'movies/show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :movies, only: %i[index show new create]
+  resources :lists, only: %i[index show new create] do
+    resources :bookmarks, only: %i[add destroy]
+  end
 end
